5238634e00bb4594c96118e16fe24b6d4b0b05ff
103047 <103047@SMSA-059-LN>
2020-08-04 18:18:26 -0500

    add Spring AOP code

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.EmployeeService;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("model");
+        context.scan("aspects");
+        context.scan("services");
+        context.scan("annotations");
+        context.scan("config");
+        context.refresh();
+
+        EmployeeService service = context.getBean(EmployeeService.class);
+        String name = "liangchen";
+        String id = "123123";
+        service.createEmployee(name, id);
+    }
+}
